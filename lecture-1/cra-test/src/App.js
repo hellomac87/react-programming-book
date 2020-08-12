3cf16f728db3eb3032478ab7c9c0558df583927b
hellomac87 <hellomac87@gmail.com>
2020-08-12 22:45:19 +0900

    1.3.5 환경변수 세팅하기

--- lecture-1/cra-test/src/App.js
+++ lecture-1/cra-test/src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./TodoList";
+import "./test.css";
 
 function App() {
+  console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
+  console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`);
   return (
     <div className="App">
       <TodoList />
