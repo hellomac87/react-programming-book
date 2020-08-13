6a1a6eb356f95c4d76c6f8196e94e0c3b018de84
hellomac87 <hellomac87@gmail.com>
2020-08-13 22:38:00 +0900

    styled-components

--- lecture-1/cra-test/src/App.js
+++ lecture-1/cra-test/src/App.js
@@ -3,13 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./TodoList";
 import "./test.css";
+import Box1 from "./Box1";
 
 function App() {
   console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
   console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`);
   return (
     <div className="App">
-      <TodoList />
+      <Box1 size={"small"} />
+      <Box1 size={"big"} />
     </div>
   );
 }
